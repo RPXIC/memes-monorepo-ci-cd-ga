dfd1c67fd6cd0bee827da0193b72116f80b4dd9d
rpxic <rpc2works@gmail.com>
2022-08-13 03:10:08 +0200

    update packages

--- webpack.config.js
+++ webpack.config.js
@@ -4,26 +4,20 @@ const webpack = require('webpack')
 
 module.exports = (env, argv) => {
   const { mode } = argv
-  const additionalPlugins = mode === 'production'
-    ? []
-    : [new webpack.HotModuleReplacementPlugin()] // Enable hot module replacement
+  const additionalPlugins = mode === 'production' ? [] : [new webpack.HotModuleReplacementPlugin()] // Enable hot module replacement
 
   const additionalEntries = mode === 'production' ? [] : ['webpack-hot-middleware/client?http://localhost:8000']
 
   return {
     mode,
-    entry: [
-      '@babel/polyfill', // so we don't need to import it anywhere
-      './client',
-      ...additionalEntries,
-    ],
+    entry: ['./client', ...additionalEntries],
     resolve: {
       alias: {
         Utilities: path.resolve(__dirname, 'client/util/'),
         Components: path.resolve(__dirname, 'client/components/'),
         Assets: path.resolve(__dirname, 'client/assets/'),
-        '@root': path.resolve(__dirname),
-      },
+        '@root': path.resolve(__dirname)
+      }
     },
     module: {
       rules: [
@@ -32,31 +26,31 @@ module.exports = (env, argv) => {
           test: /\.js$/,
           exclude: /node_modules/,
           use: {
-            loader: 'babel-loader',
-          },
+            loader: 'babel-loader'
+          }
         },
         {
           test: /\.css$/,
-          use: ['style-loader', 'css-loader'],
+          use: ['style-loader', 'css-loader']
         },
         {
           // Load other files
           test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
-          use: ['file-loader'],
-        },
-      ],
+          use: ['file-loader']
+        }
+      ]
     },
     plugins: [
       new webpack.DefinePlugin({
         'process.env.BUILT_AT': JSON.stringify(new Date().toISOString()),
-        'process.env.NODE_ENV': JSON.stringify(mode),
+        'process.env.NODE_ENV': JSON.stringify(mode)
       }),
       // Skip the part where we would make a html template
       new HtmlWebpackPlugin({
         template: 'index.html',
-        favicon: path.resolve(__dirname, 'client/assets/favicon.ico'),
+        favicon: path.resolve(__dirname, 'client/assets/favicon.ico')
       }),
-      ...additionalPlugins,
-    ],
+      ...additionalPlugins
+    ]
   }
 }
